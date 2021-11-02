8f6e08a189cead7efd926f5ba407572efca69288
Filatov_Yurii <filatov.ios.dev@gmail.com>
2021-11-02 20:29:07 +0300

    Spirograph

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Юрий Филатов on 02.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
